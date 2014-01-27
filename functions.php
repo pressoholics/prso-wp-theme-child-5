25d01882d57a49f79b7f13e684c270cd6cf22293
BENJAMIN MOODY <hireme@benjaminmoody.com>
2014-01-27 17:45:55 -0500

    initial commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/******************************************************************
+ * 	Text Domain
+ *
+ *****************************************************************/
+ define( 'PRSOTHEMEFRAMEWORK__DOMAIN', 'prso-child-theme-domain' );
+
+/**
+* ADD CUSTOM THEME FUNCTIONS HERE -----
+*
+*/
